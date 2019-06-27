f59ceef37be35caab3f0a0fce12eab0d21d14b9c
José Posada <josedanielpm@gmail.com>
2019-06-27 00:32:42 -0500

    configurations validation messages translated

--- app/Http/Requests/UpdateConfigurations.php
+++ app/Http/Requests/UpdateConfigurations.php
@@ -41,10 +41,19 @@ class UpdateConfigurations extends FormRequest
     {
         return [
             'low_capital.required' => 'El capital bajo es requerido.',
+            'low_capital.numeric' => 'El capital bajo debe ser un número.',
+
             'take_profit_limit_1.required' => 'El límite tomar ganacias 1 es requerido.',
+            'take_profit_limit_1.numeric' => 'El límite tomar ganacias 1 debe ser un número.',
+
             'take_profit_limit_2.required' => 'El límite tomar ganacias 2 es requerido.',
+            'take_profit_limit_2.numeric' => 'El límite tomar ganacias 2 debe ser un número.',
+
             'take_profit_limit_3.required' => 'El límite tomar ganacias 3 es requerido.',
+            'take_profit_limit_3.numeric' => 'El límite tomar ganacias 3 debe ser un número.',
+
             'risk.required' => 'El riesgo es requerido.',
+            'risk.numeric' => 'El riesgo debe ser un número.',
         ];
     }
 }
