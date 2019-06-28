5a22ac4da3f08754d6aeadc3e43a440af1989a37
José Posada <josedanielpm@gmail.com>
2019-06-27 23:02:24 -0500

    guared configurations admin route

--- app/Http/Controllers/Admin/ConfigurationsController.php
+++ app/Http/Controllers/Admin/ConfigurationsController.php
@@ -11,7 +11,7 @@ class ConfigurationsController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth', 'role:admin']);
     }
 
     public function index()
