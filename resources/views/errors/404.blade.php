c9e845ccd1bec5a5b9f23ddc0a59ddb523c1f17f
José Posada <josedanielpm@gmail.com>
2019-07-04 22:27:12 -0500

    added 404 custom view

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" type="image/png" href="{{ asset('img/logo.png  ') }}" />
+  
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>Página no encontrada - Moneyuca</title>
+
+  <!-- Custom fonts of the template -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="{{ asset('css/fontawesome/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Styles of the template -->
+  <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+
+  <!-- Custom styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+  <!-- Extra styles -->
+  @yield('extra-css')
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <div class="container-fluid">
+          <div class="text-center" style="height: 100vh;">
+              <div class="error mx-auto" data-text="404">404</div>
+              <p class="lead text-gray-800">Página no encontrada</p>
+              <a href="/">&larr; Volver al inicio</a>
+          </div>   
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+
+<!-- Bootstrap core JavaScript-->
+<script src="{{ asset('js/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="{{ asset('js/jquery/jquery.easing.min.js') }}"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+
+
+</body>
+
+</html>
+
+    
